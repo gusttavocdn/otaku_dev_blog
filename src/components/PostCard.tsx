eb87579462f9d0afc9832b942a78e8a652357f3b
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-02-22 12:19:05 -0300

    feat: add PostCard component

---
+++ src/components/PostCard.tsx
@@ -0,0 +1,23 @@
+import { Card, CardHeader, Heading, CardBody, Text } from '@chakra-ui/react';
+
+export function PostCard() {
+  return (
+    <Card bg='base.post' h='260px' pt={5}>
+      <CardHeader
+        color='base.title'
+        display='flex'
+        justifyContent='space-between'
+      >
+        <Heading fontSize='xl' flex={1}>
+          JavaScript data types and data structures
+        </Heading>
+        <Text as='span'>Há 1 dia</Text>
+      </CardHeader>
+      <CardBody color='base.text' pt={2}>
+        Programming langues all have built-in data structures, but these often
+        differ from one language to another. This article attempts to list the
+        built-in data strcutres. available in...
+      </CardBody>
+    </Card>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,34 @@
-import { Flex, Heading, Text, VStack, Input } from '@chakra-ui/react';
+import {
+  Flex,
+  Heading,
+  Text,
+  VStack,
+  Input,
+  Card,
+  CardHeader,
+  CardBody,
+  SimpleGrid,
+} from '@chakra-ui/react';
 import { HomeHeader } from '../components/HomeHeader';
+import { PostCard } from '../components/PostCard';
 import { SearchForm } from '../components/SearchForm';
 
 export function Home() {
   return (
     <VStack w={'100vw'} h={'100vh'}>
-      <HomeHeader />
+      <VStack w='80%' spacing={10}>
+        <HomeHeader />
 
-      <SearchForm />
+        <SearchForm />
+
+        <SimpleGrid columns={2} spacing={5} w='80%' maxW='864px'>
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+        </SimpleGrid>
+      </VStack>
     </VStack>
   );
 }
