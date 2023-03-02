abe4608a455500ece6d2e1f9d5a5577a46166ae7
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-03-02 17:43:01 -0300

    refactor: removed api key

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,12 +1,7 @@
 import axios from 'axios';
 
-const token = import.meta.env.VITE_GITHUB_TOKEN;
-
 const api = axios.create({
   baseURL: 'https://api.github.com',
-  headers: {
-    Authorization: `Bearer ${token as string}`,
-  },
 });
 
 export { api };
