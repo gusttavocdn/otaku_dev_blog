b7cc3e5a6792a50a5ffefa0206fd1cc19c69b1d9
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-03-02 16:18:17 -0300

    feat: add api key

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const token = import.meta.env.VITE_GITHUB_TOKEN;
+
 const api = axios.create({
-  baseURL: 'https://api.github.com'
+  baseURL: 'https://api.github.com',
+  headers: {
+    Authorization: `Bearer ${token as string}`,
+  },
 });
 
 export { api };
